0e5dcf1b11e877c4da3333d867456036e4fe50dc
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-13 14:21:27 +0200

    Add base glfw configuration

--- src/main.c
+++ src/main.c
@@ -3,5 +3,9 @@
 
 int main(void)
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     return 0;
 }
