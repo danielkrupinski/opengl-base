f7bd21b5b493f1be8c4a0e89d7cd94de260121da
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-13 14:27:05 +0200

    Create new glfw window

--- src/main.c
+++ src/main.c
@@ -7,5 +7,13 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+    if (!window) {
+        printf("Failed to create GLFW window!\n");
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
     return 0;
 }
